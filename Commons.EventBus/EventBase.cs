6907d9c9fe63da9bf7350f506c6d0a932cc7b7b2
truongchauhien <truongchauhien@gmail.com>
2026-05-04 10:01:22 +0700

    Changed to UUIDv7.

--- Commons.EventBus/EventBase.cs
+++ Commons.EventBus/EventBase.cs
@@ -2,15 +2,15 @@
 {
     public abstract class EventBase : IEvent
     {
-        public Guid Id { get; } = Guid.NewGuid();
+        public Guid Id { get; } = Guid.CreateVersion7();
         public DateTime CreatedAt { get; } = DateTime.UtcNow;
         public string CorrelationId { get; }
 
         public EventBase(string? correlationId)
         {
-            this.Id = Guid.NewGuid();
+            this.Id = Guid.CreateVersion7();
             this.CreatedAt = DateTime.UtcNow;
-            this.CorrelationId = correlationId ?? Guid.NewGuid().ToString();
+            this.CorrelationId = correlationId ?? Guid.CreateVersion7().ToString();
         }
     }
 }
